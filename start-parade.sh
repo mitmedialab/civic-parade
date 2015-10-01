4cc4d6c266617e78b9e5423e351aecb7ee3ac0d6
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-10-01 16:16:05 -0400

    add script to open up chrome

---
+++ start-parade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SITE_LIST=$(curl https://raw.githubusercontent.com/c4fcm/civic-parade/master/sitelist.txt)
+
+echo opening
+echo $SITELIST
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk $SITE_LIST
